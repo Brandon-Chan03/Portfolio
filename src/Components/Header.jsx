175af529eae2d4d31ecbd3140ae8cfa827895518
Brandon-Chan03 <Brandon.chan0216@gmail.com>
2022-05-13 20:29:23 -0500

    added header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1 class="logo font-oxygen">B.C.</h1>
+            <nav class="main-links font-redhat">
+                <ul>
+                    <li><a href="#about-me">About</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="./Components/contactPage.html">Contact Me</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
