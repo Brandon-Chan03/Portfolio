7de3af5297b29b92348009821ae1c5a4cbbdb4c9
Brandon-Chan03 <Brandon.chan0216@gmail.com>
2022-05-14 05:43:29 -0500

    changed list items to buttons to later deal with functionality also changed css of buttons to match prevoius list items

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,12 +2,12 @@ export function Header() {
     return (
         <header>
             <h1 className="logo font-oxygen">B.C.</h1>
-            <nav className="main-links font-redhat">
-                <ul>
-                    <li>About</li>
-                    <li>Experience</li>
-                    <li>Projects</li>
-                    <li>Contact Me</li>
+            <nav className="main-links">
+                <ul className="test">
+                    <button className='header-button'>About</button>
+                    <button className='header-button'>Experience</button>
+                    <button className='header-button'>Projects</button>
+                    <button className='header-button'>Contact Me</button>
                 </ul>
             </nav>
         </header>
